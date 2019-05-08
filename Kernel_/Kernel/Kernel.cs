28a1c6ed8de8e8de5e42b482c42b8ecfcd8cfd55
Kaique Santos <kags_@live.com>
2019-05-08 10:47:05 -0300

    Initial commit

---
+++ Kernel_/Kernel/Kernel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Nucleo;
+
+namespace Kernel
+{
+    public class OS
+    {
+
+
+        public Memory Memory { get; set; }
+        public List<Process> Processes { get; set; }
+
+        public OS()
+        {
+            Memory = new Memory(1024 * 2);
+            Processes = new List<Process>();
+        }
+
+        public void AddProcess(Process process)
+        {            
+            Processes.Add(process);
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < Processes.Count; i++)
+            {
+                Processes[i].Update(new Quantum(1));
+            }
+        }
+    }
+}
